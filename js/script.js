1d52b3ffac75fe5e100454f2a2c7153edca761af
ITdev2020 <72341716+ITdev2020@users.noreply.github.com>
2020-10-04 15:11:10 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,47 @@
+function menuOn() {
+document.getElementById("men").style.display="block";
+}
+function menuOff() {
+	document.getElementById("men").style.display="none";
+}
+
+tns({
+	container: ".slider",
+	// mode: gallery,
+	// axis: vertical,
+	gutter: 10,
+	speed: 15000,
+	autoplayTimeout: 15000,
+	items: 1,
+	autoplayHoverPause: true,
+	autoplayButtonOutput: false,
+	autoHeight: true,
+	mouseDrag: true,
+	slideBy: "page",
+	autoplay: true,
+	controls: false,
+	nav: false,
+	gutter: 5,
+	edgePadding: 400,
+	fixedWidth: 265,
+	autoWidth: true,
+	items: 1,
+	responsive: {
+		640: {
+			edgePadding: 20,
+			gutter: 20,
+			items: 2,
+			fixedWidth: 265,
+		},
+		700: {
+			gutter: 30,
+			fixedWidth: 265,
+		},
+		900: {
+			items: 3,
+			fixedWidth: 265,
+		},
+	},
+
+	// disable: true,
+});
---
+++ readme.txt
@@ -0,0 +1,15 @@
+18:33 2020-10-01
+(Lithuanian)
+Ko nespejau:
+- mygtuka "to top" button;
+- burgeriui "toggle" funkcija;
+- slider'i su paginacija;
+- navigacijoje - vietoj href - button;
+- sutvarkyti responsibility;
+- ideti daug daug komentaru anglu kalba;
+....
+
+
+Sincerely
+Igor Talaiko
++370 686 81223
