84759eef243e73953ad177dcccf6a9c23beed906
Igor <72341716+ITdev2020@users.noreply.github.com>
2020-11-24 21:12:26 +0200

    Delete footer.php

--- app/views/footer.php
+++
@@ -1,7 +0,0 @@
-<footer>
-    <div class="container">
-        <!-- panaudojam date() funkcija -->
-        <p class="copyright">&copy; <?php echo date('Y'); ?>. Visos teisės saugomos.</p>
-        <p class="copyright">&copy; 2018. Visos teisės saugomos.</p>
-    </div>
-</footer>
